272649243e81b31ccc5d4d4456ce0cede1b87377
Trần Chức Thiện <102866314+ThienNoob@users.noreply.github.com>
2024-05-25 14:35:31 +0700

    Update Appbar.js

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -10,7 +10,7 @@ export default function Appbar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Student Management
+            Students Management
           </Typography>
         </Toolbar>
       </AppBar>
